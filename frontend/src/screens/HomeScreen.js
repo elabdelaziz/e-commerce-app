a15d8eb3882ccdbd901fc72ab77483fb5bc9a800
Abdulaziz Ahmed <abdelaziz.ahmd74@gmail.com>
2021-10-06 21:00:37 +0200

    added node server

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Products from "../components/Products";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Products key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomeScreen;
